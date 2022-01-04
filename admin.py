0c4b0173d3188cf9ba1caa44dbc408c48500cb2e
Happy-Ferret <MarkBauer@gmx.ch>
2022-01-04 23:45:38 +0100

    Update admin.py
    Remove unused boilerplate.

--- admin.py
+++ admin.py
@@ -1,40 +1,7 @@
 #!/usr/bin/env python
 # pylint: disable=C0116,W0613
 
-from functools import wraps
 from telegram import Update
-
-def config_chat_only(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        update = args[2].to_dict() if not isinstance(args[2], dict) else args[2]
-        chat_id = update['message']['chat']['id']
-        if int(chat_id) != int(args[0].config.group_id):
-            return
-        f(*args, **kwargs)
-    return wrapper
-
-
-def admin_only(test: str):
-    def decorator(f):
-        @wraps(f)
-        def wrapper(*args, **kwargs):
-            print(test)
-        return wrapper
-    return decorator
-
-
-def exempt_admins():
-    def decorator(f):
-        @wraps(f)
-        def wrapper(*args, **kwargs):
-            update = args[2].to_dict() if not isinstance(args[2], dict) else args[2]
-            user_id = update['message']['from']['id']
-            if user_id in args[0].config.admin_ids:
-                return
-            f(*args, **kwargs)
-        return wrapper
-    return decorator
 
 def check_perm (update: Update):
     # Check user permission. Might be hard to generalize.

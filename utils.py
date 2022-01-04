b52d58f3b9153c761f88aba36d63ca08ecb975f5
Happy-Ferret <MarkBauer@gmx.ch>
2022-01-04 05:06:24 +0100

    Update utils.py
    Add markdown escape helper function.

--- utils.py
+++ utils.py
@@ -2,6 +2,10 @@
 # pylint: disable=C0116,W0613
 
 from telegram import Bot
+from telegram.utils.helpers import escape_markdown
 import config
 
 bot = Bot(config.TOKEN)
+
+def escape_md(text):
+    return escape_markdown(text, version=2)

7f31d100f313532b78a2a178b9b4ff1f77e3226c
Happy-Ferret <MarkBauer@gmx.ch>
2022-01-04 14:26:51 +0100

    Add version.py
    Update version string whenever master has changed.
    
    - Update about.py
      - Display version string defined in version.py.

--- about.py
+++ about.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # pylint: disable=C0116,W0613
 
+import version
+
 ANIMATION = "CgACAgQAAxkBAAEPmLNhyw5I4KQkImOVtyPruTr8AAFODwgAAoECAAI5TZVS5CTu45yOCN0jBA"
-TEXT = ("Pudding v0.0.5 \n \n" 
+TEXT = ("Pudding {} \n \n" 
         "Made with much love and German engineering ❤️ \n \n"
-        "GitHub: https://git.io/JygBe")
+        "GitHub: https://git.io/JygBe").format(version.CURRENT)
---
+++ version.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# pylint: disable=C0116,W0613
+
+CURRENT = "0.0.5-b52d58f"
